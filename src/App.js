9f2a2f1863674f10bd3d5d01996901096b76cbd5
Patryk Wernerowicz <patryk.wernerowicz1@gmail.com>
2023-09-18 22:00:53 +0200

    empty input sending fixed

--- src/App.js
+++ src/App.js
@@ -43,14 +43,16 @@ function App() {
   };
 
   const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-      },
-    ]);
+    if (content !== "") {
+      setTasks(tasks => [
+        ...tasks,
+        {
+          content,
+          done: false,
+          id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+        },
+      ]);
+    };
   };
 
   return (
